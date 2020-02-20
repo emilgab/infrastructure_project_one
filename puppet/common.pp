0c96bfe4d46e8eab7b9c84b5d5af071f986b767b
vetletm <vetlemoen@gmail.com>
2020-02-20 14:20:32 +0100

    Update common.pp to use future parsing

--- puppet/common.pp
+++ puppet/common.pp
@@ -1,12 +1,22 @@
-$groups = [ 'webadmins','sudo' ]
 $people = [ 'tom', 'brady', 'janet' ]
+$groups = [ 'webadmins','sudo' ]
 
-group { 'webadmins' :
-  ensure => present,
-  gid => 501,
+# must be run with the future parser: puppet apply --parser future --noop common.pp
+each($people) |$user| {
+        user { "$user":
+                ensure => present,
+                password => '$1$WDW0AeF1$kAQNNYlIKZ6RbqpEiPAct0',
+                shell => '/bin/bash',
+                home => "/home/$user",
+                require => File["/home/$user"],
+                groups => $groups,
+        }
+        file { "/home/$user":
+                ensure => directory
+        }
 }
 
-user { $people :
-  ensure => present,
-  groups => $groups,
+group { 'webadmins' :
+        ensure => present,
+        gid => 501,
 }
